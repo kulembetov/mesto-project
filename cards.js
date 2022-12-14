ca9facd7d3839748c8a74400e0e7eb2df80f9ae7
Artur Kulembetov <artur_kulembetov@mail.ru>
2022-12-14 14:49:11 +0500

    The second commit

---
+++ cards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+    {
+      name: 'Торонто',
+      link: 'https://images.unsplash.com/photo-1514924013411-cbf25faa35bb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=690&q=80',
+    },
+    {
+      name: 'Токио',
+      link: 'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1194&q=80',
+    },
+    {
+      name: 'Буэнос-Айрес',
+      link: 'https://images.unsplash.com/photo-1576541572620-cbc5457517b4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+    },
+    {
+      name: 'Москва',
+      link: 'https://images.unsplash.com/photo-1604324561454-5d28e384aebc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
+    },
+    {
+      name: 'Дубай',
+      link: 'https://images.unsplash.com/photo-1575538439014-1b8bc5fcaa1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+    },
+    {
+      name: 'Сан-Франциско',
+      link: 'https://images.unsplash.com/photo-1534050359320-02900022671e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
+    }
+  ];
